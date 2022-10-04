18ca8f8d734c33ea84496b77a26b6dc64a8d2233
lorenzo <2306227+xab13r@users.noreply.github.com>
2022-10-04 11:12:19 +0100

    Finish writing TextComms class, needs doubling and testing

--- lib/text_comms.rb
+++ lib/text_comms.rb
@@ -1,8 +1,8 @@
 require_relative 'tokens'
-require 'net/http'
+require 'twilio-ruby'
 
 class TextComms
-	def initiate(to_number)
+	def initialize(to_number)
 		@to_number = to_number
 		credentials = TwilioCredentials.new
 		@auth_token = credentials.auth_token
@@ -10,15 +10,32 @@ class TextComms
 		@from_number = credentials.from_number
 	end
 
-	def send_confirmation_message
-		client = Twilio::REST::Client.new(account_sid, auth_token)
+	def delivery_time
+		# Allowing for delivery in within 70mins
+		return (Time.now + 4200).strftime("%H:%M")
+	end
 
-		delivery_time = (Time.now + 4200).strftime("%k:%m")
+	def send_text_confirmation
+		client = Twilio::REST::Client.new(@account_sid, @auth_token)
+		#p client
+
+		text_body = "Order confirmed! It will be delivered before #{delivery_time}"
 
 		client.messages.create(
 			from: @from_number,
 			to: @to_number,
-			body: "Order confimerd! It will be delivered before #{delivery_time}"
+			body: text_body
 		)
+
+		# Fetch last message sent
+		messages = client.messages.list(limit: 1)
+		message_sid = messages[0].sid
+		# Get last message body to compare to text_body
+		message_body =  messages[0].body
+		# Due to limitation with Twilio trial account
+		actual_text_body = "Sent from your Twilio trial account - " + text_body
+		# Check last message sent is correctly
+		return message_body == actual_text_body
+
 	end
 end
--- spec/text_comms_spec.rb
+++ spec/text_comms_spec.rb
@@ -1,8 +1,20 @@
 require 'text_comms'
 
 RSpec.describe TextComms do
-	it "can send a text with the delivery time using the twilio API" do
-		text_comms = TextComms.new()
-		#TextComms.send_confirmation_message()
+	context "at the beginning" do
+		it "can initialize" do
+			text_comms = TextComms.new('+447400048424')
+		end
+
+		it "can calculate delivery time accurately" do
+			text_comms = TextComms.new('+447400048424')
+			expect(text_comms.delivery_time).to eq (Time.now + 4200).strftime("%H:%M")
+		end
+
+		it "can send a confirmation message with delivery time" do
+			text_comms = TextComms.new('+447400048424')
+			expect(text_comms.send_text_confirmation).to eq true
+		end
+
 	end
 end
