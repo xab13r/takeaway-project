5fea3058b8d0ab0dc0af8065b68327ef45b83bf9
lorenzo <2306227+xab13r@users.noreply.github.com>
2022-10-04 12:20:55 +0100

    Wrote mocking tests for TextComms

--- lib/text_comms.rb
+++ lib/text_comms.rb
@@ -2,12 +2,14 @@ require_relative 'tokens'
 require 'twilio-ruby'
 
 class TextComms
-	def initialize(to_number)
-		@to_number = to_number
+	def initialize(twilio_client)
+
 		credentials = TwilioCredentials.new
 		@auth_token = credentials.auth_token
 		@account_sid = credentials.account_sid
 		@from_number = credentials.from_number
+
+		@twilio_client = twilio_client
 	end
 
 	def delivery_time
@@ -15,27 +17,16 @@ class TextComms
 		return (Time.now + 4200).strftime("%H:%M")
 	end
 
-	def send_text_confirmation
-		client = Twilio::REST::Client.new(@account_sid, @auth_token)
-		#p client
+	def send_text_confirmation(number)
+		# client = Twilio::REST::Client.new(@account_sid, @auth_token)
+		#client = @requester.new(@account_sid, @auth_token)
 
 		text_body = "Order confirmed! It will be delivered before #{delivery_time}"
 
-		client.messages.create(
+		@twilio_client.messages.create(
 			from: @from_number,
-			to: @to_number,
+			to: number,
 			body: text_body
 		)
-
-		# Fetch last message sent
-		messages = client.messages.list(limit: 1)
-		message_sid = messages[0].sid
-		# Get last message body to compare to text_body
-		message_body =  messages[0].body
-		# Due to limitation with Twilio trial account
-		actual_text_body = "Sent from your Twilio trial account - " + text_body
-		# Check last message sent is correctly
-		return message_body == actual_text_body
-
 	end
 end
--- spec/text_comms_spec.rb
+++ spec/text_comms_spec.rb
@@ -12,9 +12,43 @@ RSpec.describe TextComms do
 		end
 
 		it "can send a confirmation message with delivery time" do
-			text_comms = TextComms.new('+447400048424')
-			expect(text_comms.send_text_confirmation).to eq true
-		end
+			fake_twilio = FakeTwilio.new('1','2')
+			text_comms = TextComms.new(fake_twilio)
+			text_comms.send_text_confirmation("0700000000")
 
+			expect(fake_twilio.messages.counter_value).to eq 1
+			#expect(text_comms.send_text_confirmation).to eq true
+		end
+	end
+end
+
+class FakeTwilio
+	def initialize(account_sid, auth_token)
+		@fake_messages = FakeMessages.new
+		p "Fake messages:"
+		p @fake_messages
+	end
+
+	def messages
+		p "Messages method called"
+		p @fake_messages
+		return @fake_messages
+	end
+end
+
+class FakeMessages
+	def initialize
+		@counter = 0
+		p "FakeMessages initialized"
+	end
+
+	def create(from:, to:, body:)
+		@counter += 1
+		p "Create method called"
+	end
+
+	def counter_value
+		p "Counter method called"
+		return @counter
 	end
 end
